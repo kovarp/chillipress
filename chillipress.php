62807b0508c983e227b615622e795da99da63be9
Pavel Kovář <kovarp95@gmail.com>
2018-12-26 15:40:42 +0100

    Loader added

---
+++ chillipress.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types = 1);
+
+new kovarp\ChilliPress\Security\HideBackend();
+new kovarp\ChilliPress\Core\Updates();
+new kovarp\ChilliPress\Theme\Setup();
+new kovarp\ChilliPress\Theme\Seo();
+new kovarp\ChilliPress\Admin\Topbar();
+new kovarp\ChilliPress\Admin\Dashboard();
+
+$container->run();
